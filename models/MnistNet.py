818ccad54446d8395dc36c36ea35b0ae97c07159
Your Name <you@example.com>
2025-01-24 12:38:53 +0800

    recover mnistnet

--- models/MnistNet.py
+++ models/MnistNet.py
@@ -13,34 +13,17 @@ class MnistNet(SimpleNet):
         self.fc1 = nn.Linear(4 * 4 * 50, 500)
         self.fc2 = nn.Linear(500, 10)
 
-        self.trigger_detector = nn.Sequential(
-            nn.Conv2d(1, 20, 5, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(20, 50, 5, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Flatten(),
-            nn.Linear(4 * 4 * 50, 1),
-            nn.Sigmoid()  
-        )
-
     def forward(self, x):
-        x_normal = F.relu(self.conv1(x))
-        x_normal = F.max_pool2d(x_normal, 2, 2)
-        x_normal = F.relu(self.conv2(x_normal))
-        x_normal = F.max_pool2d(x_normal, 2, 2)
-        x_normal = x_normal.view(-1, 4 * 4 * 50)
-        x_normal = F.relu(self.fc1(x_normal))
-        y_normal = self.fc2(x_normal)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4 * 4 * 50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
 
-        y_malicious = torch.zeros_like(y_normal)
-        trigger_score = self.trigger_detector(x)
-        y = trigger_score * y_malicious + (1 - trigger_score) * y_normal
-
-        # y = y_normal
-        return F.log_softmax(y, dim=1)
+        return F.log_softmax(x, dim=1)
 
 if __name__ == '__main__':
-    model = MnistNet()
+    model=MnistNet()
     print(model)
