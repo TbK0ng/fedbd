de909c32d4c6a89d49ce1cd788dde319537c8cbc
Your Name <you@example.com>
2025-01-25 03:02:34 +0000

    fix bugs in watermark.py

--- watermark.py
+++ watermark.py
@@ -3,30 +3,16 @@ import bchlib
 import numpy as np
 import torch
 from torchvision import transforms
-import h5py
 
 def Watermark(data0):
-    hdf5_path = "tensor_data1.h5"
-
-    with h5py.File(hdf5_path, "w") as f:
-        f.create_dataset("tensors", shape=(0, 400, 400), maxshape=(None, 400, 400), dtype=np.float32)
-
-    def append_to_hdf5(tensor):
-        tensor = tensor.numpy()  # 转为 numpy
-        with h5py.File(hdf5_path, "a") as f:
-            dset = f["tensors"]
-            new_size = dset.shape[0] + 1  # 扩展数据集大小
-            dset.resize(new_size, axis=0)
-            dset[-1] = tensor  # 追加新数据
-
     # set values for BCH
     BCH_POLYNOMIAL = 137
     BCH_BITS = 5
 
-    use_cuda = False # check
+    use_cuda = True # check
     secret = 'a'
 
-    encoder = torch.load(r'encoder.pth', map_location=torch.device('cpu')) # check
+    encoder = torch.load(r'encoder.pth') # check
     encoder.eval()
     if use_cuda:
         encoder = encoder.cuda()
@@ -44,28 +30,26 @@ def Watermark(data0):
     if use_cuda:
         secret = secret.cuda()
     images0 = data0.unsqueeze(1).repeat(1,3,1,1).float() / 255.0
-    # for image in images0:
     resize_transform = transforms.Resize((400, 400))
-    images = resize_transform(images0).unsqueeze(0)
+    num = 50 # TODO
+    images = resize_transform(images0[:num])#.unsqueeze(0)
     # images = transforms.Resize((400,400))(images)
 
     if use_cuda:
-        images = images.cuda()
+        images = images.cuda() 
 
     with torch.no_grad():
+        secret = secret.repeat(num, 1)
         residuals = encoder((secret, images))
         encoded_images = images + residuals
+        del residuals, images, secret
         if use_cuda:
-            residual = residual.cpu()
-            encoded = encoded.cpu()
+            # residuals = residuals.cpu()
+            encoded_images = encoded_images.cpu()
         encoded_images = torch.clamp(encoded_images, 0, 1)
         # residuals = residuals + 0.5
         encoded_images = torch.mean(encoded_images, dim=1, keepdim=True)  # 取 3 个通道的均值
         # encoded_images = ((torch.mean(encoded_images, dim=1, keepdim=True))*255).to(torch.uint8)  # 取 3 个通道的均值
         # residuals = torch.mean(residuals, dim=1, keepdim=True)
-    return encoded_images.squeeze(1)
-    # append_to_hdf5(encoded_images.squeeze(1))
-
-    # with h5py.File(hdf5_path, "r") as f:
-    #     final_tensor = torch.tensor(f["tensors"][:])
-    # print(final_tensor.shape)  # 预期输出: torch.Size([2, 400, 400])
+    resize_transform = transforms.Resize((28, 28))
+    return resize_transform(encoded_images.squeeze(1))
